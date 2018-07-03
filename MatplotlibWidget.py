a5b0cff2fe9911d32f2c8034949f80e549a0be0f
Alexander Liptak <alexadner.liptak.jr@googlemail.com>
2018-07-03 14:09:43 +0100

    Updated GUI files (minor changes)

--- MatplotlibWidget.py
+++ MatplotlibWidget.py
@@ -4,12 +4,12 @@ from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
 
 class MatplotlibWidget(QtGui.QWidget):
-    def __init__(self, parent=None, *args, **kwargs):
-        super(MatplotlibWidget, self).__init__(parent)
-        self.figure = Figure(*args, **kwargs)
-        self.canvas = FigureCanvas(self.figure)
-        self.toolbar = NavigationToolbar(self.canvas, self)
-        layout = QtGui.QVBoxLayout()
-        layout.addWidget(self.toolbar)
-        layout.addWidget(self.canvas)
-        self.setLayout(layout)
+	def __init__(self, parent=None, *args, **kwargs):
+		super(MatplotlibWidget, self).__init__(parent)
+		self.figure = Figure(*args, **kwargs)
+		self.canvas = FigureCanvas(self.figure)
+		self.toolbar = NavigationToolbar(self.canvas, self)
+		layout = QtGui.QVBoxLayout()
+		layout.addWidget(self.toolbar)
+		layout.addWidget(self.canvas)
+		self.setLayout(layout)
