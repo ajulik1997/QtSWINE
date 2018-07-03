7960e24d4bbd814eca793295fb6fe121928e1c8f
Alexander Liptak <alexadner.liptak.jr@googlemail.com>
2018-07-03 14:35:52 +0100

    Added pyinstaller-based compiler

---
+++ compile.bat
@@ -0,0 +1,8 @@
+C:\mcstas-2.4.1\miniconda3\Scripts\pyinstaller main.py ^
+	--noconfirm ^
+	--clean ^
+	--onefile ^
+	--name="QtSWINE" ^
+	--add-data="default.instr;instr" ^
+	--icon="icon.ico" ^
+	--uac-admin
